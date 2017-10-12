05824ef1e0ec82e9b59f2f1bcb8254e2649f8901
Zygal <jnovor@comcast.net>
2017-10-12 19:58:16 -0400

    Update DNA.c

--- DNA.c
+++ DNA.c
@@ -22,8 +22,6 @@ void reallArray(Array *a, size_t initialSize) {
 	a->size = initialSize;
 }
 void insertArray(Array *a, int element) {
-	// a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
-	// Therefore a->used can go up to a->size
 	if (a->used == a->size) {
 		a->size *= 2;
 		a->array = (int *)realloc(a->array, a->size * sizeof(int));
